ee51e6b664dbd181554ed1c1d9730f0054b0aae9
RaihanAkbar2903 <raihan.as293@gmail.com>
2024-02-29 19:51:15 +0700

    edit home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,21 +8,30 @@
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
 <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 </head>
-<body class="bg-gray-300">
-    <nav class="p-5 bg-sky-700 md:flex md:items-center md:justify-between">
+<body class="bg-gray-200">
+    <nav class="p-5 bg-teal-500 md:flex md:items-center md:justify-between">
         <div>
-            <span class="text-2xl font-[Poppins] text-white shadow">
+            <img src="../img/logo.jpeg" alt="">
+            <span class="text-2xl font-[Poppins] text-white">
                 BookLand
             </span>
         </div>
-</div>
-        <input type="search" name="" id="" placeholder="Cari Buku, Novel, Komik, Penulis " class="rounded-full px-5 py-5 w-96">
+</div>  
+<div>
+<select name="cars" class="bg-teal-500 text-white shadow mr-20 text-2xl font-[Poppins]">
+  <option value="volvo">Kategori</option>
+  <option value="saab">Fiksi</option>
+  <option value="mercedes">Non Fiksi</option>
+  <option value="audi">Majalah</option>
+</select>
+        <input type="search" name="" id="" placeholder="Cari Buku, Novel, Komik, Penulis " class="rounded-full px-5 py-5 w-96 bg-gray-200">
+        </div>
         <ul class="md:flex md:items-center">
             <li class="mx-4">
-                <a href="" class="text-xl text-white shadow hover:text-cyan-500 duration-500">MASUK</a>
+                <a href="" class="text-xl text-white shadow hover:text-gray-600 duration-500 font-[Poppins]">MASUK</a>
             </li>
             <li class="mx-4">
-            <ion-icon name="bag-handle-outline" class="text-white shadow size-10"></ion-icon>
+            <a href=""><ion-icon name="bag-handle-outline" class="text-white shadow size-10"></ion-icon></a>
             </li>
         </ul>
     </nav>
