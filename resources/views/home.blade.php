61ca902c2f85af69e64752b5429fe10132b3d146
RaihanAkbar2903 <raihan.as293@gmail.com>
2024-03-07 19:32:30 +0700

    50% dashbord

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,45 +8,41 @@
 <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body class="bg-slate-500">
+<body class="bg-stone-100">
 
     <!-- NAVBAR -->
 
-    <nav class="bg-gradient-to-br from-stone-500 via-orange-200 to-slate-500 dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
-  <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-  <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
-      <img src="img/logo.png" class="h-8" alt="Flowbite Logo">
-      <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">BookLand</span>
-  </a>
-  <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-       <a href="login"><button type="button" class="bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-40% to-teal-900-500 to-90% bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Masuk</button></a>
-      <button data-collapse-toggle="navbar-sticky" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
-        <span class="sr-only">Open main menu</span>
-        <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
-        </svg>
-    </button>
-  </div>
-  <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
-    <select name="" id="" class="bg-transparent mr-5"> 
-      <option value="">KATEGORI</option>
-      <option value="">Fiksi</option>
-      <option value="">Non Fiksi</option>
-      <option value="">Manga</option>
-      <option value="">Manhwa</option>
-    </select>
-    <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-      <li>
-        <input type="search" class="rounded-lg w-72" placeholder="Cari Buku, Novel, Komik, Penulis ">
-      </li>
-    </ul>
-  </div>
-  </div>
-</nav>
+    <nav class="p-5 bg-gradient-to-br from-stone-500 via-orange-200 to-slate-500 md:flex md:items-center md:justify-between">
+        <div class="md:flex md:items-center md:justify-between">
+        <img src="img/logo.png" alt="" width="100">
+            <span class="text-2xl font-[Poppins] text-white">
+                BookLand
+            </span>
+        </div>
+</div>  
+<div>
+<select name="cars" class="bg-orange-200 text-white shadow mr-20 text-2xl font-[Poppins]">
+  <option value="volvo">Kategori</option>
+  <option value="saab">Fiksi</option>
+  <option value="mercedes">Non Fiksi</option>
+  <option value="audi">Majalah</option>
+  <option value="audi">Manga</option>
+</select>
+        <input type="search" name="" id="" placeholder="Cari Buku, Novel, Komik, Penulis " class="rounded-full px-5 py-5 w-96 bg-stone-100">
+        </div>
+        <ul class="md:flex md:items-center">
+            <li class="mx-4">
+                <a href="" class="text-xl text-white shadow hover:text-gray-600 duration-500 font-[Poppins]">MASUK</a>
+            </li>
+            <li class="mx-4">
+            <a href=""><ion-icon name="bag-handle-outline" class="text-white shadow size-10"></ion-icon></a>
+            </li>
+            <li>
+            </li>
+        </ul>
+    </nav>
 
     <!-- NAVBAR -->
-
-    <!-- Carousel -->
 
     <!-- Carousel -->
  
@@ -89,7 +85,6 @@
 
     <!-- Carousel -->
 
-    <!-- Carousel -->
 
 
     <!-- Card -->
@@ -156,6 +151,141 @@
 </div>
 <hr class="my-20 border-t-4"/>
     <!-- Card -->
+
+    <!-- Footer -->
+    <div>
+    <footer class="bg-gradient-to-br from-stone-500 via-orange-200 to-slate-500 mt-96">
+    <div class="container mx-auto py-14 px-6">
+        <div class="grid md:grid-cols-12 grid-cols-1 gap-7">
+            <div class="lg:col-span-4 col-span-12">
+            <h5 class="tracking-wide text-black font-semibold">Tentang Kami</h5>
+                <a href="/">
+                <img'
+                class="h-12"
+                src:
+                alt=""
+                />
+                </a>
+                <p class="nt-6">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus incidunt illum, deserunt voluptatem quaerat dolores pariatur veritatis autem quia libero temporibus voluptate aut praesentium? Nesciunt consequuntur quis unde repellat neque.
+                </p>
+            </div>
+            <div class="lg:col-span-2 md:col-span-4 col-span-12">
+            <h5 class="tracking-wide text-black font-semibold">Company</h5>
+            <ul class="list-none mt-6 space-y-2">
+                <li>
+                    <a
+                    href="#"
+                    class="hover:text-white transition-all duration-500 ease-in-out"
+                    >
+                    About us </a
+                    >
+                </li>
+                <li>
+                    <a
+                    href="#"
+                    class="hover:text-white transition-all duration-500 ease-in-out"
+                    >
+                    services </a
+                    >
+                </li>
+                <li>
+                    <a
+                    href="#"
+                    class="hover:text-white transition-all duration-500 ease-in-out"
+                    >
+                    pricing </a
+                    >
+                </li>
+                <li>
+                    <a
+                    href="#"
+                    class="hover:text-white transition-all duration-500 ease-in-out"
+                    >
+                    Blog </a
+                    >
+                </li>
+            </div>
+            <div class="lg:col-span-3 md:col-span-4 col-span-12">
+                <h5 class="tracking-wide text-black font-semibold">
+                    important Links
+                </h5>
+                <ul class="list-none space-y-2 mt-6">
+                    <li>
+                        <a
+                        href="#"
+                        class="hover:text-white transition-all duration-500 ease-in-out"
+                        >
+                        terms of servies</a
+                        >
+                    </li>
+                    <li>
+                        <a
+                        href="#"
+                        class="hover:text-white transition-all duration-500 ease-in-out"
+                        >
+                        Privacy Policy</a
+                        >
+                    </li>
+                    <li>
+                        <a
+                        href="#"
+                        class="hover:text-white transition-all duration-500 ease-in-out"
+                        >
+                        Documentation</a
+                        >
+                    </li>
+                   </div>
+                   <div class="lg:col-span-3 md:col-span-4 col-span-12">
+                <h5 class="tracking-wide text-black font-semibold">
+                    Hubungi Kami
+                </h5>
+                <ul class="list-none space-y-2 mt-6">
+                    <li>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M14.25 9.75v-4.5m0 4.5h4.5m-4.5 0 6-6m-3 18c-8.284 0-15-6.716-15-15V4.5A2.25 2.25 0 0 1 4.5 2.25h1.372c.516 0 .966.351 1.091.852l1.106 4.423c.11.44-.054.902-.417 1.173l-1.293.97a1.062 1.062 0 0 0-.38 1.21 12.035 12.035 0 0 0 7.143 7.143c.441.162.928-.004 1.21-.38l.97-1.293a1.125 1.125 0 0 1 1.173-.417l4.423 1.106c.5.125.852.575.852 1.091V19.5a2.25 2.25 0 0 1-2.25 2.25h-2.25Z" />
+                    </svg>
+                        <a
+                        href="#"
+                        class="hover:text-white transition-all duration-500 ease-in-out"
+                        >
+
+                        +62 83495103974</a
+                        >
+                    </li>
+                    <li>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
+                 </svg>
+
+                        <a
+                        class="hover:text-white transition-all duration-500 ease-in-out"
+                        
+                        >
+                        BookLand@gmail.com</a
+                        >
+                    </li>
+
+        
+        
+    <div class="md:text-center text-center container flex items-center justify-center mx-auto py-7 px-6">
+        <p class="mb-0 text-lg">
+            &copy;
+            <script>
+                document.write(new Date().getFullYear());
+            </script>
+            BookLand team
+        </p>
+                    </div>
+
+
+</div>
+</div>
+</div>
+</footer>
+</div>
+
+<!-- footer -->
     
 </body>
 </html>
